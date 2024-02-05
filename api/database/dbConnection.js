340790e93b9f042274c3a39cd58e9c5d3fa3b134
Arnaud Wafo <81981692+Netal97@users.noreply.github.com>
2024-02-05 02:55:38 +0100

    Update dbConnection.js

--- api/database/dbConnection.js
+++ api/database/dbConnection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'zitrone',
+    password: '',
     database: 'webapp_eventplanner',
 });
 
